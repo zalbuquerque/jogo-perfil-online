ef759deb4053470f5bbdeb39db90573506361d0b
viniciusdsouza <vduartesantiago@gmail.com>
2025-04-06 19:19:36 -0300

    feat: add function to fetch API

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
+async function fetchData() {
+    const loadingElement = document.getElementById('loading');
+    const categoryElement = document.getElementById('category');
+    const answerElement = document.getElementById('answer');
+    const cluesElement = document.getElementById('clues');
+    try {
+        const response = await fetch("https://chronic-tansy-viniciusdsouza-labs-254a6bc6.koyeb.app/get-card")
 
+        loadingElement.style.display = 'block';
+
+        if (!response.ok) throw new Error("Erro ao buscar dados");
+
+        const data = await response.json();
+
+        categoryElement.innerText = data.category;
+        answerElement.innerText = data.name;
+        cluesElement.innerHTML = '';
+
+        data.clues.forEach((clue, i) => {
+            const p = document.createElement('p');
+            p.innerText = `${i + 1} - ${clue}`;
+            cluesElement.append(p);
+        });
+    } catch (error) {
+        console.error(error);
+    } finally {
+        loadingElement.style.display = 'none';
+    }
+}
+
+fetchData();
